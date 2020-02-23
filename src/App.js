b3270f206b35027b4420b6b17bdc43f0aa34e44d
kitaiki <oh0425@gmail.com>
2020-02-24 00:24:13 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { userState, useState } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: '리엑트 기초 배우기1',
+      checked: true,
+    },
+    {
+      id:2,
+      text: '리엑트 기초 배우기2',
+      checked: true,
+    },
+    {
+      id:3,
+      text: '리엑트 기초 배우기3',
+      checked: true,
+    }
+  ])
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
