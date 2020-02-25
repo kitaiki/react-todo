9110c761dd581061f33291255ca712e5ad27d1fd
kitaiki <oh0425@gmail.com>
2020-02-25 17:32:25 +0900

    add update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,11 @@ import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 
-function TodoList({ todos }) {
+function TodoList({ todos, onRemove, onToggle }) {
     return (
         <div className="TodoList">
             {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} />
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
             ))}
             
         </div>
