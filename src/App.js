927a739adf598ac5f4df120a4e7936085f01d33c
kitaiki <oh0425@gmail.com>
2020-02-26 23:06:34 +0900

    React.memo 최적화

--- src/App.js
+++ src/App.js
@@ -4,27 +4,25 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+
+function createBulkTodos() {
+  const array = [];
+  for(let i=1; i<=2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+
+  return array;
+}
+
 function App() {
 
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: '리엑트 기초 배우기1',
-      checked: true,
-    },
-    {
-      id:2,
-      text: '리엑트 기초 배우기2',
-      checked: true,
-    },
-    {
-      id:3,
-      text: '리엑트 기초 배우기3',
-      checked: false,
-    }
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
@@ -34,26 +32,26 @@ function App() {
         checked: false,
       };
 
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
     },
-    [todos],
+    [],
   );
 
   const onRemove = useCallback(id => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos(todos => todos.filter(todo => todo.id !== id));
   }, 
-  [todos],
+  [],
   );
 
   const onToggle = useCallback(id => {
-    setTodos(
+    setTodos(todos =>
       todos.map(
         todo => todo.id === id ? {...todo, checked: !todo.checked} : todo,
       ),
     );
   },
-  [todos],
+  [],
   );
 
   return (
